98bf6aef0410642874ef15ef6e355379283be75c
rcMarty <korotwitschkamartin@gmail.com>
2026-05-13 02:04:25 +0200

    not null in migrations

--- migrations/20260512213321_rep.sql
+++ migrations/20260512213321_rep.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS reputation_logs
 (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    giver_id    TEXT    NOT NULL,
-    receiver_id TEXT    NOT NULL,
-    rep_value   INTEGER NOT NULL,
-    reason      TEXT    NOT NULL,
+    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    giver_id    TEXT                              NOT NULL,
+    receiver_id TEXT                              NOT NULL,
+    rep_value   INTEGER                           NOT NULL,
+    reason      TEXT                              NOT NULL,
     created_at  DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
